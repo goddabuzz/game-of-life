0925b81586cab4ea6b0db381d6827befdabb6a42
Daniel Kloosterman <buz.i286@gmail.com>
2012-10-25 09:34:53 +0200

    Game of life

---
+++ background.js
@@ -0,0 +1,65 @@
+/**
+   Background class =)
+   Author: Daniel Kloosterman
+   Git: https://github.com/goddabuzz/game-of-life
+ */
+function Background(game){
+  this.game = game;
+  this.container = game.container;
+  
+  try {
+    this.canvas = document.createElement('canvas');
+    this.container.appendChild(this.canvas);
+    this.context = this.canvas.getContext("2d");
+  } catch (ex) {
+    throw Error('Could not create background. ' + ex);
+  }
+  
+  this.updateSizes();
+  this.draw();
+}
+
+Background.prototype.updateSizes = function(){
+  this.isResized = true;
+  this.canvas.width = this.game.width;
+  this.canvas.height = this.game.height;
+}
+
+Background.prototype.draw = function(){
+  var context = this.context;
+  var game = this.game;
+  
+  if (!this.isResized || game.borderSize === 0){
+    return;
+  }
+  
+  // Clear old 
+  context.clearRect(0,0,game.width,game.height);
+  
+  var size = (game.cellSize+game.borderSize);
+  
+  // Style
+  context.strokeStyle = "rgba(0,0,0,.4)";
+  context.lineWidth = game.borderSize;
+  
+  context.beginPath();
+  
+    context.moveTo(0,0);
+    // Draw horizontal lines
+    for (var x = 0; x < game.cellCount[1]+2; x++){
+      context.lineTo(game.width, x * size);
+      context.moveTo(0, (x+1) * size);
+    }
+    
+    context.moveTo(0,0);
+    // Draw vertical lines
+    for (var y = 0; y < game.cellCount[0]+2; y++) {
+      context.lineTo(y * size, game.height);
+      context.moveTo((y + 1) * size, 0);
+    }
+    
+  context.closePath();
+  context.stroke();
+  
+  this.isResized = false;
+}
---
+++ control.js
@@ -0,0 +1,75 @@
+
+function Control(game){
+  this.game = game;
+  
+  var container = game.container;
+  
+  // Create control box
+  var div = document.createElement('div');
+  div.className = 'control';
+  container.appendChild(div);
+  this.div = div;
+  
+  this.fps=0;
+  
+  // Statistics
+  this.fpsDiv = this.addStats('FPS: - ');
+  this.population = this.addStats('Population: - ')
+  this.iterations = this.addStats('Iterations: - ')
+  this.time = this.addStats('Time: - ');
+
+  this.startBtn = this.addButton('Start', this.onButtonClick);
+  this.clearBtn = this.addButton('Clear', this.onClearClick);
+  this.randomBtn = this.addButton('Random', this.onRandomClick);
+}
+
+Control.prototype.addStats = function(text){
+  var div = document.createElement('div');
+  div.innerHTML = text||'';
+  this.div.appendChild(div);
+  return div;
+}
+
+Control.prototype.addButton = function(text, handler){
+  var btn = document.createElement('button');
+  btn.innerHTML = text;
+  btn.onclick = eventHandler(handler, this);
+  this.div.appendChild(btn);
+  return btn;
+}
+
+Control.prototype.onButtonClick = function(evt){
+  if (this.game.timer){
+    this.game.stop()
+    this.startBtn.innerHTML = 'Start'
+  } else {
+    this.game.start();
+    this.startBtn.innerHTML = 'Stop'
+  }
+}
+
+Control.prototype.onClearClick = function(){
+  this.game.clear();
+}
+
+Control.prototype.onRandomClick = function(){
+  this.game.random();
+}
+
+Control.prototype.setFPS = function(fps){
+  this.fps += (fps - this.fps) / 50
+  this.fpsDiv.innerHTML = 'FPS: ' + Math.round(this.fps);
+}
+
+Control.prototype.setPopulation = function(population){
+  this.population.innerHTML = 'Population: ' + population;
+}
+
+Control.prototype.setIterations = function(iterations){
+  this.iterations.innerHTML = 'Iterations: ' + iterations;
+}
+
+Control.prototype.setTime = function(time){
+  time = time || 0;
+  this.time.innerHTML = 'Time: ' + Math.round(time/1000);
+}
